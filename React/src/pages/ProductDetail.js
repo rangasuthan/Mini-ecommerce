3ae28ced2c0169f30f4120cb8d051eb49c99051f
T SRI RANGASUTHAN <114223685+rangasuthan@users.noreply.github.com>
2025-02-08 13:53:06 +0530

    Update ProductDetail.js

--- React/src/pages/ProductDetail.js
+++ React/src/pages/ProductDetail.js
@@ -1,7 +1,6 @@
 import {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 
-
 export default function ProductDetail({cartItems, setCartItems}) {
     const [product, setProduct] = useState(null);
     const [qty, setQty] = useState(1);
