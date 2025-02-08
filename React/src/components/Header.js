d9b6e607898522395ed5aa317373eb672bc2e393
T SRI RANGASUTHAN <114223685+rangasuthan@users.noreply.github.com>
2025-02-08 13:10:36 +0530

    Update Header.js

--- React/src/components/Header.js
+++ React/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header({cartItems}) {
     return <nav className="navbar row">
             <div className="col-12 col-md-3">
                 <div className="navbar-brand">
-                <Link to="/"> <img width="150px" src="/images/logo.jpg" /></Link>
+                <Link to="/"> <img width="150px" src="./images/logo.jpg" /></Link>
                 </div>
             </div>
 
