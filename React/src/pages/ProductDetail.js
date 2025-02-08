98d96fff3d36c55fb98a23042ed178dccac04599
T SRI RANGASUTHAN <114223685+rangasuthan@users.noreply.github.com>
2025-02-08 14:26:42 +0530

    Update ProductDetail.js

--- React/src/pages/ProductDetail.js
+++ React/src/pages/ProductDetail.js
@@ -42,7 +42,7 @@ export default function ProductDetail({cartItems, setCartItems}) {
     return  product && <div className="container container-fluid">
                 <div className="row f-flex justify-content-around">
                     <div className="col-12 col-lg-5 img-fluid" id="product_image">
-                        <img src={require(`./../components/images/products/${product.images[0].image}`)} alt="sdf" height="500" width="500" />
+                        <img src={require(`./../components/${product.images[0].image}`)} alt="sdf" height="500" width="500" />
                     </div>
 
                     <div className="col-12 col-lg-5 mt-5">
